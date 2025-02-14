7a63237aeb2ed58735a4e167a58ed0006f302ab4
Barış Gündüz <baris@gunduzmedya.com>
2025-02-14 10:48:10 +0300

    Layout a to link update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,56 +2,88 @@
 
 import "../app/globals.css";
 import { useEffect, useState } from "react";
-import { HomeIcon, BookOpenIcon, RocketLaunchIcon, MapIcon, GiftIcon } from "@heroicons/react/24/outline";
+import {
+	HomeIcon,
+	BookOpenIcon,
+	RocketLaunchIcon,
+	MapIcon,
+	GiftIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [isReady, setIsReady] = useState(false);
+	const [isReady, setIsReady] = useState(false);
 
-  useEffect(() => {
-    setIsReady(true);
-  }, []);
+	useEffect(() => {
+		setIsReady(true);
+	}, []);
 
-  return (
-    <html lang="en">
-      <body className={`${isReady ? "flex flex-col md:flex-row bg-background text-textPrimary min-h-screen" : ""}`}>
-        {/* SOL: Menü */}
-        <aside className={`w-full md:w-1/4 h-auto md:h-screen bg-foreground shadow-md p-6 flex md:flex-col md:fixed border-r border-borderColor`}>
-          {/* Logo */}
-          <div className="flex items-center mb-4 md:mb-6">
-            <HomeIcon className="h-8 w-8 text-textPrimary mr-2" />
-            <h1 className="text-2xl font-bold text-textPrimary">Teeny Tiny Web</h1>
-          </div>
+	return (
+		<html lang="en">
+			<body
+				className={`${
+					isReady
+						? "flex flex-col md:flex-row bg-background text-textPrimary min-h-screen"
+						: ""
+				}`}
+			>
+				{/* SOL: Menü */}
+				<aside
+					className={`w-full md:w-1/4 h-auto md:h-screen bg-foreground shadow-md p-6 flex md:flex-col md:fixed border-r border-borderColor`}
+				>
+					{/* Logo */}
+					<div className="flex items-center mb-4 md:mb-6">
+						<HomeIcon className="h-8 w-8 text-textPrimary mr-2" />
+						<h1 className="text-2xl font-bold text-textPrimary">
+							Teeny Tiny Web
+						</h1>
+					</div>
 
-          {/* Menü */}
-          <nav className="flex flex-row md:flex-col space-x-4 md:space-x-0 md:space-y-4 w-full">
-            <a href="/" className="flex items-center text-textPrimary hover:text-accent">
-              <HomeIcon className="h-5 w-5 mr-2" />
-              <span>Home</span>
-            </a>
-            <a href="/docs" className="flex items-center text-textPrimary hover:text-accent">
-              <BookOpenIcon className="h-5 w-5 mr-2" />
-              <span>Docs</span>
-            </a>
-            <a href="/projects" className="flex items-center text-textPrimary hover:text-accent">
-              <RocketLaunchIcon className="h-5 w-5 mr-2" />
-              <span>Projects</span>
-            </a>
-            <a href="/roadmaps" className="flex items-center text-textPrimary hover:text-accent">
-              <MapIcon className="h-5 w-5 mr-2" />
-              <span>Roadmaps</span>
-            </a>
-            <a href="/freebie" className="flex items-center text-textPrimary hover:text-accent">
-              <GiftIcon className="h-5 w-5 mr-2" />
-              <span>Freebie</span>
-            </a>
-          </nav>
-        </aside>
+					{/* Menü */}
+					<nav className="flex flex-row md:flex-col space-x-4 md:space-x-0 md:space-y-4 w-full">
+						<Link
+							href="/"
+							className="flex items-center text-textPrimary hover:text-accent"
+						>
+							<HomeIcon className="h-5 w-5 mr-2" />
+							<span>Home</span>
+						</Link>
+						<Link
+							href="/docs"
+							className="flex items-center text-textPrimary hover:text-accent"
+						>
+							<BookOpenIcon className="h-5 w-5 mr-2" />
+							<span>Docs</span>
+						</Link>
+						<Link
+							href="/projects"
+							className="flex items-center text-textPrimary hover:text-accent"
+						>
+							<RocketLaunchIcon className="h-5 w-5 mr-2" />
+							<span>Projects</span>
+						</Link>
+						<Link
+							href="/roadmaps"
+							className="flex items-center text-textPrimary hover:text-accent"
+						>
+							<MapIcon className="h-5 w-5 mr-2" />
+							<span>Roadmaps</span>
+						</Link>
+						<Link
+							href="/freebie"
+							className="flex items-center text-textPrimary hover:text-accent"
+						>
+							<GiftIcon className="h-5 w-5 mr-2" />
+							<span>Freebie</span>
+						</Link>
+					</nav>
+				</aside>
 
-        {/* SAĞ: İçerik Alanı */}
-        <main className="w-full md:w-3/4 p-6 md:ml-[25%] h-screen overflow-y-auto">
-          {children}
-        </main>
-      </body>
-    </html>
-  );
+				{/* SAĞ: İçerik Alanı */}
+				<main className="w-full md:w-3/4 p-6 md:ml-[25%] h-screen overflow-y-auto">
+					{children}
+				</main>
+			</body>
+		</html>
+	);
 }
