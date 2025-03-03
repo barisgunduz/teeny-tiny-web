7e35c6642d39c9eae6dfe7391a0c66503444961e
barisgunduz <baris@gunduzmedya.com>
2025-03-04 00:07:23 +0300

    Next config update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ❌ Build sırasında ESLint hatalarını yoksay
-  },
-  typescript: {
-    ignoreBuildErrors: true, // ❌ Build sırasında TypeScript hatalarını yoksay
-  },
+	eslint: {
+		ignoreDuringBuilds: true, // ❌ Build sırasında ESLint hatalarını yoksay
+	},
+	typescript: {
+		ignoreBuildErrors: true, // ❌ Build sırasında TypeScript hatalarını yoksay
+	},
+	generateBuildId: async () => {
+		return `${Date.now()}`; // 🚀 Her build'de farklı ID oluştur
+	},
 };
 
 export default nextConfig;
